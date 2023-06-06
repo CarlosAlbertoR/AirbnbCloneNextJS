242e4126dc955944f7fff946338fc5f9407e7465
Carlos Alberto R <carlosalbertorios3@gmail.com>
2023-06-05 23:43:50 -0500

    🌞 Addition of categories at the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import { Nunito } from "next/font/google";
+import { lazy } from "react";
 
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import LoginModal from "./components/modals/LoginModal";
-import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
 import "./globals.css";
 import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ["latin"] });
+const LoginModal = lazy(() => import("./components/modals/LoginModal"));
+const RegisterModal = lazy(() => import("./components/modals/RegisterModal"));
+const ToasterProvider = lazy(() => import("./providers/ToasterProvider"));
 
 export const metadata = {
   title: "Airbnb",
